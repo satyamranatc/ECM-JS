4c01d11cc32853a27c3efeefbfd296098938d33f
Satyam Rana <57563008+satyamranatc@users.noreply.github.com>
2024-06-04 12:16:42 +0530

    News App

--- Product.js
+++ Product.js
@@ -1,24 +1,59 @@
+let Main = document.querySelector("main");
+let Filter = document.getElementById("FilterProducts");
 
-let Main = document.querySelector('main');
-async function GetProducts()
-{
-    let data = await fetch("https://fakestoreapi.com/products")
-    data = await data.json();
-    console.log(data)
+let data = "";
+async function GetProducts() {
+  data = await fetch("https://fakestoreapi.com/products");
+  data = await data.json();
 
-    Main.innerHTML = "";
-    data.forEach(element => {
-        Main.innerHTML += `
-        <div class="ProductCard">
-        <img src="${element.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${element.title}</h5>
-          <p class="card-text">${element.description}</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
-      </div>
-      `
-    });
+  for (let i of data) {
+    console.log(i.category);
+  }
+
+  Display(data);
 }
 
+Filter.addEventListener("change", (e) => {
+  if (e.target.value == "Fashion") {
+    NewData = data.filter(
+      (e) => e.category == "women's clothing" || e.category == "men's clothing"
+    );
+    console.log(NewData);
+  }
+
+  else if(e.target.value == "All")
+  {
+    NewData = data;
+    console.log(NewData);
+  }
+  else if(e.target.value == "Electronics")
+    {
+      NewData = data.filter((e) => e.category == "electronics");
+      console.log(NewData);
+    }
+
+  else if(e.target.value == "Jewelery")
+  {
+    NewData = data.filter((e) => e.category == "jewelery");
+    console.log(NewData);
+  }
+  Display(NewData);
+});
+
 GetProducts();
+
+function Display(data) {
+  Main.innerHTML = "";
+  data.forEach((element) => {
+    Main.innerHTML += `
+      <div class="ProductCard">
+      <img src="${element.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h4 class="card-title">${element.title}</h4>
+        <p class="card-text">${element.description}</p>
+        <button>Buy Now</button>
+      </div>
+    </div>
+    `;
+  });
+}
